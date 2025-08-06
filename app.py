bfe2bca9d842f500302c9d3430ff4c0ad363a300
Daniel <danyochoa112@gmail.com>
2025-08-05 23:44:03 -0600

    feat: diseño de sistema para leer entradas desde un .txt

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from logic.inputs import read_content_txt
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    input = read_content_txt("./inputs/entradas.txt") #Se leen las entradas del archivo
+    return render_template("index.html", input=input)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ logic/inputs.py
@@ -0,0 +1,17 @@
+#Funcion que recibe una ruta o nombre de archivo y retorna cada linea de este archivo
+def read_content_txt(path: str):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        print(f"Error: El archivo '{path}' no fue encontrado.")
+        return None
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        return None
+    
+#Funcion que recibe una entrada, un string y retorna un array de str con las lineas
+def split_input(input: str):
+    lines = input.splitlines()
+    return lines
